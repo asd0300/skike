bb57d338fefc5665b515858899237b1b211fff71
asd0300 <50284529+asd0300@users.noreply.github.com>
2021-12-16 10:14:56 +0800

    remove crawler proto type

--- 20211013_project_crawler/server/utils/util.py
+++
@@ -1,5 +0,0 @@
-import os
-def dir_last_updated(folder):
-    return str(max(os.path.getmtime(os.path.join(root_path, f))
-                   for root_path, dirs, files in os.walk(folder)
-                   for f in files))
